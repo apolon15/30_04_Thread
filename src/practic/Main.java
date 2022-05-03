0ef6be35ff660a9e4703279f0013578365edc81b
apolon15 <ya.apolon15@yandex.ru>
2022-05-03 16:53:05 +0300

    исправил еточность

--- src/practic/Main.java
+++ src/practic/Main.java
@@ -22,24 +22,27 @@ public class Main {
 арифметическое возвращаются в метод main, где должны
 быть отображены.
  */
-//        MyThread1 myThread1 = new MyThread1();
-//        myThread1.setName("CREATMASS");
-//        myThread1.start();
-//        try {
-//            myThread1.join();
-//            MyThread1 myThread2 = new MyThread1();
-//            MyThread1 myThread3 = new MyThread1();
-//            myThread2.setName("AVG");
-//            myThread2.start();
-//            System.out.println("Среднее значение в массиве " + myThread2.avg(myThread1.getMass()));
-//            myThread2.join();
-//            myThread3.setName("SUM");
-//            myThread3.start();
-//            System.out.println("Cумма чисел в массиве " + myThread3.sum(myThread1.getMass()));
-//            System.out.println(Arrays.toString(myThread1.getMass()));
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        MyThread1 myThread1 = new MyThread1();
+        myThread1.setName("CREATMASS");
+        myThread1.start();
+        try {
+            myThread1.setMass(myThread1.getMass());
+            myThread1.join();
+            MyThread1 myThread2 = new MyThread1();
+            MyThread1 myThread3 = new MyThread1();
+            myThread2.setName("AVG");
+            myThread3.setName("SUM");
+            myThread2.setMass(myThread1.getMass());
+            myThread3.setMass(myThread1.getMass());
+            myThread2.start();
+            myThread3.start();
+
+            // System.out.println("Среднее значение в массиве " + myThread2.avg(myThread1.getMass()));
+            // System.out.println("Cумма чисел в массиве " + myThread3.sum(myThread1.getMass()));
+            System.out.println(Arrays.toString(myThread1.getMass()));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 /*
 При старте приложения попросите пользователя ввести 2 дроби.
 Затем используя интерфейс Runnable запустить 4 потока,
@@ -50,35 +53,35 @@ public class Main {
 Деление двух дробей.
 Результ операций нужно вывести в консоль.
  */
-        Scanner scanner = new Scanner(System.in);
-        boolean stop;
-        double numX = 0;
-        double numY = 0;
-        do {
-            try {
-                System.out.println("Введи 1 дробь через запятую \" , \": ");
-                numX = Double.parseDouble(scanner.next());
-                System.out.println("Введи 2 дробь через запятую \" , \": ");
-                numY = Double.parseDouble(scanner.next());
-                stop = true;
-            } catch (NullPointerException exception) {
-                System.out.println("Не корректный ввод");
-                stop = false;
-            } catch (NumberFormatException exception) {
-                System.out.println("Не корректный ввод");
-                stop = false;
-            }
-        } while (!stop);
-        double finalNumX = numX;
-        double finalNumY = numY;
-        Thread threadRunnable = new Thread(() -> System.out.printf("Сумма дробей равна: %.1f \n", (finalNumX + finalNumY)));
-        Thread threadRunnable2 = new Thread(() -> System.out.printf("Разность дробей равна: %.1f \n ", (finalNumX - finalNumY)));
-        Thread threadRunnable3 = new Thread(() -> System.out.print("Произведение дробей равно: " + ((finalNumX * finalNumY))+"\n"));
-        Thread threadRunnable4 = new Thread(() -> System.out.print("Разность при делении дробей равна: " + ((finalNumX / finalNumY))+"\n"));
-        threadRunnable.start();
-        threadRunnable2.start();
-        threadRunnable3.start();
-        threadRunnable4.start();
+//        Scanner scanner = new Scanner(System.in);
+//        boolean stop;
+//        double numX = 0;
+//        double numY = 0;
+//        do {
+//            try {
+//                System.out.println("Введи 1 дробь через запятую \" , \": ");
+//                numX = Double.parseDouble(scanner.next());
+//                System.out.println("Введи 2 дробь через запятую \" , \": ");
+//                numY = Double.parseDouble(scanner.next());
+//                stop = true;
+//            } catch (NullPointerException exception) {
+//                System.out.println("Не корректный ввод");
+//                stop = false;
+//            } catch (NumberFormatException exception) {
+//                System.out.println("Не корректный ввод");
+//                stop = false;
+//            }
+//        } while (!stop);
+//        double finalNumX = numX;
+//        double finalNumY = numY;
+//        Thread threadRunnable = new Thread(() -> System.out.printf("Сумма дробей равна: %.1f \n", (finalNumX + finalNumY)));
+//        Thread threadRunnable2 = new Thread(() -> System.out.printf("Разность дробей равна: %.1f \n ", (finalNumX - finalNumY)));
+//        Thread threadRunnable3 = new Thread(() -> System.out.print("Произведение дробей равно: " + ((finalNumX * finalNumY))+"\n"));
+//        Thread threadRunnable4 = new Thread(() -> System.out.print("Разность при делении дробей равна: " + ((finalNumX / finalNumY))+"\n"));
+//        threadRunnable.start();
+//        threadRunnable2.start();
+//        threadRunnable3.start();
+//        threadRunnable4.start();
 //        ExecutorService executorService = Executors.newFixedThreadPool(4);
 //        executorService.submit(threadRunnable);
 //        executorService.submit(threadRunnable2);
--- src/practic/MyThread1.java
+++ src/practic/MyThread1.java
@@ -5,13 +5,12 @@ import java.util.OptionalDouble;
 
 public class MyThread1 extends Thread {
     private int[] mass;
-    private double avgMass;
-    private int sum;
+   // private double avgMass;
+ //   private int sum;
 
     public MyThread1() {
 
     }
-
     public void setMass(int[] mass) {
         this.mass = mass;
     }
@@ -19,7 +18,6 @@ public class MyThread1 extends Thread {
     public int[] getMass() {
         return mass;
     }
-
     public int[] creatMass() {
         int[] mass = new int[10];
         for (int i = 0; i < mass.length; i++) {
@@ -28,16 +26,18 @@ public class MyThread1 extends Thread {
         setMass(mass);
         return mass;
     }
-
     @Override
     public void run() {
         if ("CREATMASS".equals(Thread.currentThread().getName())) {
             creatMass();
-        } else {
+        }
+        if ("AVG".equals(Thread.currentThread().getName())) {
+            System.out.println("Среднее значение в массиве " + avg(getMass()));
 
+        }if ("SUM".equals(Thread.currentThread().getName())) {
+            System.out.println("Cумма чисел в массиве "+sum(getMass()));
         }
     }
-
 
     public int sum(int[] mass) {
         return Arrays.stream(mass).sum();
